ee0a3193d55ceb4f68227ea7d3898f5162a3c158
romatsypin <romatsypin@gmail.com>
2021-06-16 12:27:22 +0300

    Refactoring. Added logging.

--- file-manager.py
+++ file-manager.py
@@ -1,16 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 import os
 import shutil
+import datetime
 
-os.chdir("C:/Users/romat/Downloads")
+downloads = "C:/Users/romat/Downloads"
+desktop = "C:/Users/romat/Desktop/"
+os.chdir(downloads)
 
-for file in os.listdir("C:/Users/romat/Downloads"):
-    if file[-3:] == "exe":
-        os.remove(file)
-    elif file[-3:] == "png" or file[-3:] == "jpg":
-        shutil.move(file,  "C:/Users/romat/Desktop/pics/" + file)
-    elif file[-3:] == "pdf":
-        shutil.move(file,  "C:/Users/romat/Desktop/pdfs/" + file)
+log_file = open(desktop + "filelog.txt", "a")
+
+def delete_file(file):
+    log_file.write(file + " is deleted at " + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "\n")
+    os.remove(file)
+    
+def move_file(file, folder):
+    log_file.write(file + " is moved at " + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "\n")
+    shutil.move(file,  desktop + folder + "/" + file)
+
+for file in os.listdir(downloads):
+    ext3 = file[-3:]
+    if ext3 == "exe":
+        delete_file(file)
+    elif ext3 == "png" or file[-3:] == "jpg":
+        move_file(file, "pics")
+    elif ext3 == "pdf":
+        move_file(file, "pdfs")
+
+log_file.close()
