5e5b2b285069eef2510d5609b60ac3914abc1fa4
romatsypin <romatsypin@gmail.com>
2021-09-23 15:13:33 +0100

    Added extensions

--- file-manager.py
+++ file-manager.py
@@ -4,7 +4,8 @@ import datetime
 
 downloads = "C:/Users/romat/Downloads"
 desktop = "C:/Users/romat/Desktop/"
-time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+dirlist = [downloads]
+time = datetime.datetime.now().strftime("%Y%m%d")
 os.chdir(downloads)
 
 log_file = open(desktop + "filelog.txt", "a")
@@ -23,13 +24,21 @@ def move_file(file, folder):
     write_log(file = file, action = "move")
     shutil.move(file,  desktop + folder + "/" + file)
 
-for file in os.listdir(downloads):
-    ext3 = file[-3:]
-    if ext3 == "exe":
-        delete_file(file)
-    elif ext3 == "png" or file[-3:] == "jpg":
-        move_file(file, "pics")
-    elif ext3 == "pdf":
-        move_file(file, "pdfs")
+for directory in dirlist:
+    for file in os.listdir(directory):
+        ext2 = file[-2:]
+        ext3 = file[-3:]
+        ext4 = file[-4:]
+        if ext3 == "exe" or ext3 == "msi" or ext3 == "zip" or ext3 == "mkv" or ext3 == "iso" or ext3 == "MP4" or ext2 == "js":
+            delete_file(file)
+        elif ext3 == "png" or ext3 == "jpg" or ext4 == "jfif":
+            move_file(file, "pics")
+        elif ext3 == "pdf":
+            move_file(file, "pdfs")
+        elif ext4 == "docx":
+            move_file(file, "words")
+        elif ext4 == "pptx":
+            move_file(file, "presentations")
+        
 
 log_file.close()
